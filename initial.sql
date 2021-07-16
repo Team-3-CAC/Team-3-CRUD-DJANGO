f031412464ea548da488c508850afa0cec298a01
CaminoFernando <camino.fernando@gmail.com>
2021-07-16 18:21:39 -0300

    update

--- initial.sql
+++ initial.sql
@@ -3,3 +3,7 @@
 -- -----------------------------------------------------
 CREATE SCHEMA IF NOT EXISTS `django` DEFAULT CHARACTER SET utf8 ;
 USE `django` ;
+
+GRANT USAGE ON *.* TO 'userdjango'@'%';
+
+GRANT ALL PRIVILEGES ON *.* TO 'userdjango'@'%';
