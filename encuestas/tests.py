dec0babd4c376a014dcad501fcc9fa6c64d12027
CaminoFernando <camino.fernando@gmail.com>
2021-07-16 18:22:27 -0300

    update

--- encuestas/tests.py
+++ encuestas/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
 
 # Create your tests here.
+
+class MyIntegrationTest(TestCase):
+
+    def setUp(self) -> None:
+        '''
+            Inicializa el set de pruebas.
+        '''
+        return super().setUp()
+    
+    def testHelloWrold(self):
+        request = self.client.get("/users/")
+        self.assertEqual(request.content , "Hello world from Django for Codo a Codo 4.0")
+
+    def testSegundoHelloWorld(self):
+        self.fail()
