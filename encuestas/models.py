e5e7aa99e2e38d76f180c0e789f9204819d5d270
CaminoFernando <camino.fernando@gmail.com>
2021-07-16 21:48:35 -0300

    update.clase.16.7.21

--- encuestas/models.py
+++ encuestas/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    username = models.CharField(max_length=32) # Acabo de craer una tabla que tiene una property que es un var char de 32 caracteres
+    dni = models.PositiveIntegerField()
